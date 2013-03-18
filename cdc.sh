164c2385535c6168226fa50861076c262ac80d8b
foohey <offim.r@gmail.com>
2013-03-18 18:36:03 +0100

    Bash shebang

--- cdc.sh
+++ cdc.sh
@@ -1,16 +1,18 @@
- #        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
- #                    Version 2, December 2004 
+#!/usr/bin/env bash
 
- # Copyright (C) 2013 Mathieu Robardey <offim.r@gmail.com> 
-
- # Everyone is permitted to copy and distribute verbatim or modified 
- # copies of this license document, and changing it is allowed as long 
- # as the name is changed. 
-
- #            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
- #   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
-
- #  0. You just DO WHAT THE FUCK YOU WANT TO.
+#        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#                    Version 2, December 2004 
+#
+# Copyright (C) 2013 Mathieu Robardey <offim.r@gmail.com> 
+#
+# Everyone is permitted to copy and distribute verbatim or modified 
+# copies of this license document, and changing it is allowed as long 
+# as the name is changed. 
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 DEFAULT_MODULES=(sessions registrations passwords unlocks confirmations)
 DEFAULT_SCOPE=users
